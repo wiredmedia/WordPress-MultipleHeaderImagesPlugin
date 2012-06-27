026da620950c020b32596a35d56a80da1dd4654a
Carl Hughes <carl.hughes@wiredmedia.co.uk>
2012-06-27 16:08:22 +0100

    updated uninstall file

--- uninstall.php
+++ uninstall.php
@@ -3,3 +3,18 @@
 if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ){
   exit ();
 }
+
+// delete option
+delete_option( 'default_header_images' );
+
+/*
+ * delete individual post meta
+ */
+$posts_with_meta = new WP_Query(array(
+  'post_status' => 'any',
+  'posts_per_page' => -1,
+  'meta_key' => '_header_images'
+));
+while ( $posts_with_meta->have_posts() ) : $posts_with_meta->the_post();
+  delete_post_meta(get_the_ID(), '_header_images');
+endwhile; wp_reset_postdata();
